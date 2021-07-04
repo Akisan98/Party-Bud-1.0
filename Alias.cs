1b46b2512f56a45b0615fcb3b3f1d274a86e8a4b
Akisan <51326806+Akisan98@users.noreply.github.com>
2021-07-04 19:12:17 +0200

    Party Bud

---
+++ Alias.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Party_Bud_1._0
+{
+    class Alias
+    {
+        // Global Ref to Stopwatch
+        Stopwatch stopWatch = new Stopwatch();
+        int timeLimit = 90; // Hardcoded for now
+
+        // Method for setting up the Game
+        public void setup()
+        {
+            Console.WriteLine("Starting Alias!");
+            Console.WriteLine("Round Rule is .......");
+        }
+
+        // Method for generating random rule in game
+        void randomRule()
+        {
+            Random rnd = new(); //  new Random(); -- > new();
+            int num = rnd.Next(1, 6);
+
+            if (num % 5 == 0)
+            {
+                stopWatch.Stop();
+                Console.WriteLine("\nRANDOM DRINKING RULE (Time Paused)\n");
+                pressKey();
+                stopWatch.Start();
+            }
+        }
+
+        // Method to get next question
+        void getQuestion()
+        {
+            randomRule();
+            Console.WriteLine("\nWrite WORD TO GUESS here");
+        }
+
+        // Method to wait for user
+        void pressKey()
+        {
+            Console.WriteLine("\nPress a Key to Continue...\n");
+            Console.ReadKey(true);
+        }
+
+        // Method to call to start game / main method.
+        public void start()
+        {
+            // Quick Info to user
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Press R for Correct Answers");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Press W for Wrong Answers\n");
+            Console.ResetColor();
+
+            pressKey();
+
+            stopWatch.Start();
+            getQuestion();
+
+            // Get time
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Counters to keep track
+            int wrongCounter = 0;
+            int rightCounter = 0;
+
+            // While limit is not reached
+            while (ts.TotalSeconds < timeLimit)
+            {
+                if (Console.KeyAvailable)
+                {
+                    // Check for input Right / Wrong
+                    Console.WriteLine();
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    switch (key.KeyChar)
+                    {
+                        case 'r':
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Correct!");
+                            Console.ResetColor();
+                            rightCounter++;
+                            getQuestion();
+                            break;
+
+                        case 'w':
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Wrong!");
+                            Console.ResetColor();
+                            wrongCounter++;
+                            getQuestion();
+                            break;
+                    }
+                }
+
+
+                ts = stopWatch.Elapsed;
+
+                // Print time
+                string elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
+                    ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("\rTime: " + elapsedTime);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+
+            // Summary of Round once done
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nNumber of Correct Question {rightCounter} ");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Number of wrong Questions {wrongCounter} ");
+            Console.ResetColor();
+
+            stopWatch.Stop();
+
+            // Calls itself to start next round!
+            // start();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Party_Bud_1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                mainMenu();
+            }
+        }
+
+        // Main Menu for the program
+        static void mainMenu()
+        {
+            string projectName = "Party Bud";
+            string creator = "Akisan";
+            string version = "1.0.0";
+            string lastModifyed = "04.07.2021";
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            // Introduction / Credits
+            Console.WriteLine($"{projectName} by {creator} - " +
+                $"Version {version} | Last Modifyed {lastModifyed}");
+
+            Console.ResetColor();
+
+            Console.WriteLine("\nPick a mode\n1. Alias\n2. Question Game\n3. Turnament Manager\n4. Quit\n");
+
+            Console.Write(" > ");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int option = Int32.Parse(userInput);
+                switch (option)
+                {
+                    case 1:
+                        runAliasGame();
+                        break;
+                    case 2:
+                        runQuestionGame();
+                        break;
+                    case 3:
+                        Console.WriteLine("Case 3");
+                        break;
+                    case 4:
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Invalid input from user is not Int, Error: {e.Message}");
+            }
+        }
+
+        // Method to start Alias game
+        static void runAliasGame()
+        {
+            Alias game = new(); // new Alias(); --> new(); 
+            game.setup();
+            Console.WriteLine("\nPress a Key to Continue...\n");
+            Console.ReadKey(true);
+            game.start();
+        }
+
+        static void runQuestionGame()
+        {
+            Console.WriteLine("Question Game!\n\nPress q to Quit\nPress n for Next Question!");
+
+            Random rnd = new(); //  new Random(); -- > new();
+            int num = rnd.Next(1, 1000); // Say there is 1000 Questions
+
+            Console.WriteLine($"\nRANDOM QUESTION {num}");
+
+            while (true)
+            {
+                num = rnd.Next(1, 1000);
+
+                if (Console.KeyAvailable)
+                {
+                    // Check for input Right / Wrong
+                    Console.WriteLine();
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    switch (key.KeyChar)
+                    {
+                        case 'q':
+                            return;
+
+                        case 'n':
+                            Console.WriteLine($"RANDOM QUESTION {num}");
+                            break;
+                    }
+                }
+            }
+        }
+
+        static void runTurnamentManager()
+        {
+
+        }
+    }
+}
